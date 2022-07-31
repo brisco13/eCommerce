adb7e8964a741b8dd6c7e93518a3dd5b68743258
Brisco Arechederra <brisco.arch@gmail.com>
2022-07-30 19:48:20 -0600

    tag routes complete

--- models/index.js
+++ models/index.js
@@ -9,9 +9,21 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: "ProductTag" });
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "Tags",
+});
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: "ProductTag" });
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "Products",
+});
 
 module.exports = {
   Product,
